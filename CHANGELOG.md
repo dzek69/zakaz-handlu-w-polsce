c668c5427e635edacffff63c78a1e629521bd1c8
dzek <git-public@dzek.eu>
2025-01-06 13:17:14 +0100

    + 2025 ~ deps update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [EZEZ Changelog](https://ezez.dev/changelog/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [1.3.0] - 2025-01-06
+### Added
+- 2025
+### Dev
+- deps update
+
 ## [1.2.0] - 2024-02-12
 ### Added
 - 2024
--- src/dates.config.js
+++ src/dates.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines */
 const DATES = {
     2020: {
         allowed: {
@@ -162,6 +163,59 @@ const DATES = {
                 11: true,
             },
             12: {
+                25: true,
+                26: true,
+            },
+        },
+        short: {},
+    },
+    2025: {
+        allowed: {
+            1: {
+                26: true,
+            },
+            4: {
+                13: true,
+                27: true,
+            },
+            6: {
+                29: true,
+            },
+            8: {
+                31: true,
+            },
+            12: {
+                7: true,
+                14: true,
+                21: true,
+            },
+        },
+        disallowed: {
+            1: {
+                1: true,
+                6: true,
+            },
+            4: {
+                20: true,
+                21: true,
+            },
+            5: {
+                1: true,
+                3: true,
+            },
+            6: {
+                8: true,
+                19: true,
+            },
+            8: {
+                15: true,
+            },
+            11: {
+                1: true,
+                11: true,
+            },
+            12: {
+                24: true,
                 25: true,
                 26: true,
             },
